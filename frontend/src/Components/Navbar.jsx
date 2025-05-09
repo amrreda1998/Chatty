c45f9a80ec740b68043964457cdc9d204f1e11ee
Amr Reda <amr.redab1398@gmail.com>
2025-05-10 00:36:10 +0300

    fix : fixed navbar path trying to fix the delpoyment build error

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
-import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { Link } from 'react-router-dom';
+import { useAuthStore } from '../store/useAuthStore';
+import { LogOut, MessageSquare, Settings, User } from 'lucide-react';
 
-const Navbar = () => {
+const TempNavbar = () => {
   const { logout, authUser } = useAuthStore();
 
   return (
@@ -13,7 +13,10 @@ const Navbar = () => {
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
@@ -23,7 +26,7 @@ const Navbar = () => {
 
           <div className="flex items-center gap-2">
             <Link
-              to={"/settings"}
+              to={'/settings'}
               className={`
               btn btn-sm gap-2 transition-colors
               
@@ -35,7 +38,7 @@ const Navbar = () => {
 
             {authUser && (
               <>
-                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
+                <Link to={'/profile'} className={`btn btn-sm gap-2`}>
                   <User className="size-5" />
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
@@ -52,4 +55,4 @@ const Navbar = () => {
     </header>
   );
 };
-export default Navbar;
+export default TempNavbar;
